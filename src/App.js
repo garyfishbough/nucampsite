57039640ee197efcba1d113911efcbe2e1b883fe
elfish <garyfish1968@comcast.net>
2022-05-01 21:06:12 -0700

    Through workshop task 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NucampLogo from "./app/assets/img/logo.png";
 import "./App.css";
 import { Container, Navbar, NavbarBrand } from "reactstrap";
+import CampsitesList from "./features/campsites/CampsiteList.js";
 
 function App() {
     return (
@@ -13,7 +14,7 @@ function App() {
                     </NavbarBrand>
                 </Container>
             </Navbar>
-            I'm ready for workshop!
+            <CampsitesList />
         </div>
     );
 }
