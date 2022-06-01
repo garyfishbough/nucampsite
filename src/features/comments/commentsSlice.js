d6754a0a8284e75020288cc317ca358fdbf92ef0
elfish <41020376+garyfishbough@users.noreply.github.com>
2022-06-01 09:46:38 -0700

    Update through wk-5 Workship task 1 (comments)

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,32 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { COMMENTS } from "../../app/shared/COMMENTS";
+import { baseUrl } from "../../app/shared/baseUrl"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+//import { COMMENTS } from "../../app/shared/COMMENTS";
+
+export const fetchComments = createAsyncThunk("comments/fetchComments", async () => {
+    const response = await fetch(baseUrl + "comments")
+    if (!response.ok) {
+        return Promise.reject("Unable to fech, status: " + response.status)
+    }
+    const data = await response.json()
+    return data
+})
 
 const initialState = {
-    commentsArray: COMMENTS,
-};
+    commentsArray: [],
+    isLoading: true,
+    errMsg: "",
+}
 
 const commentsSlice = createSlice({
-    name: "comments",
+    name: "partners",
     initialState,
-    reducers: {
-        addComment: (state, action) => {
-            console.log("addComment action.payload", action.payload);
-            console.log("addComment state.commentsArray", state.commentsArray);
-            const newComment = {
-                id: state.commentsArray.length + 1,
-                ...action.payload,
-            };
-            state.commentsArray.push(newComment);
+    reducers: {},
+    extraReducers: {
+        [fetchComments.pending]: (state) => {
+            state.isLoading = true
+        },
+        [fetchComments.fulfilled]: (state, action) => {
+            state.isLoading = false
+            state.errMsg = ""
+            state.commentsArray = action.payload
+        },
+        [fetchComments.rejected]: (state, action) => {
+            state.isLoading = false
+            state.errMsg = action.error ? action.error.message : "Fetch failed"
         },
     },
-});
+})
 
-export const commentsReducer = commentsSlice.reducer;
+export const commentsReducer = commentsSlice.reducer
 
-export const { addComment } = commentsSlice.actions;
+export const { addComment } = commentsSlice.actions
 
 export const selectCommentsByCampsiteId = (campsiteId) => (state) => {
     return state.comments.commentsArray.filter(
         (comment) => comment.campsiteId === parseInt(campsiteId)
-    );
-};
+    )
+}
